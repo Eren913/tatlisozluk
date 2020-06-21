fe15e7a1f1ff6b6659b9bdfe2c3cf79cbf83bf21
eren çelik <elik.dark91>
2020-06-21 23:38:29 +0300

    Fikir Clasıı Eklendi

--- tatlisozluk/Controller/Anafikir.swift
+++ tatlisozluk/Controller/Anafikir.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class Anafikir: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sgmntKategori: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
         
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableView.automaticDimension
     }
-    
-
 
 }
+extension Anafikir : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+
 
---
+++ tatlisozluk/Model/Fikir.swift
@@ -0,0 +1,30 @@
+//
+//  Fikir.swift
+//  tatlisozluk
+//
+//  Created by lil ero on 21.06.2020.
+//  Copyright © 2020 lil ero. All rights reserved.
+//
+
+import Foundation
+
+class Fikir{
+    
+    private(set) var kullaniciAdi :String!
+    private(set) var eklenmeTarihi : Date!
+    private(set) var fikirText : String!
+    private(set) var yorumSayisi : Int!
+    private(set) var begeniSayisi : Int!
+    private(set) var documentId : String!
+    
+    
+    init(kullaniciAdi : String,eklenmeTarihi : Date, fikirText : String, yorumSayisi : Int,begeniSayisi : Int,documentId : String){
+        
+        self.kullaniciAdi = kullaniciAdi
+        self.eklenmeTarihi = eklenmeTarihi
+        self.fikirText = fikirText
+        self.yorumSayisi = yorumSayisi
+        self.begeniSayisi = begeniSayisi
+        self.documentId = documentId
+    }
+}
